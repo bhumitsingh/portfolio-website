55815b025ffa90afd22df66527a72cf74cb6f5f5
Bhumit Singh <chiragsingh360@gmail.com>
2025-10-06 16:33:05 +0530

    new ui

--- assets/README.md
+++
@@ -1,2 +0,0 @@
-# portfolio-website
-This website contains all my code, article links and projects.
---
+++ tracklore-labs/app/page.tsx
@@ -0,0 +1,9 @@
+import { motion } from 'framer-motion';
+
+export default function Landing() {
+  return (
+    <div>
+      Landing
+    </div>
+  )
+}
